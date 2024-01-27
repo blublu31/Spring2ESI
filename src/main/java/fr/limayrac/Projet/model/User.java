a88cda42b5629e54a3ddffe48f5d42a0cb3fab6f
blublu31 <clement.bluzat@limayrac.fr>
2024-01-27 14:54:46 +0100

    pom + user & userRepo

---
+++ src/main/java/fr/limayrac/Projet/model/User.java
@@ -0,0 +1,21 @@
+package fr.limayrac.Projet.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Add other necessary fields, relationships, and annotations
+}
